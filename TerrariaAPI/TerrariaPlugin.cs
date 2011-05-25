89770f2cd7042d3740460fa98967aae81be4d3d6
Fox-Face <germano_--@hotmail.com>
2011-05-25 00:47:35 -0300

    Added PluginInfo struct Edit TerrariaPlugin class, added Load(), Unload() and constructors

--- TerrariaAPI/TerrariaPlugin.cs
+++ TerrariaAPI/TerrariaPlugin.cs
@@ -5,7 +5,45 @@ using System.Text;
 
 namespace TerrariaAPI
 {
+    public struct PluginInfo
+    {
+        public string Name;
+        public string Version;
+        public string Author;
+        public string Description;
+    }
+
     class TerrariaPlugin
     {
+        public PluginInfo PluginInfo;
+
+        public TerrariaPlugin(PluginInfo info)
+        {
+            PluginInfo = info;
+        }
+
+        public TerrariaPlugin(string Name, string Version, string Author, string Description)
+        {
+            PluginInfo.Name = Name;
+            PluginInfo.Version = Version;
+            PluginInfo.Description = Description;
+            PluginInfo.Author = Author;
+        }
+
+        /// <summary>
+        /// Plugin initialization should be done in this function
+        /// </summary>
+        public virtual void Load()
+        {
+
+        }
+
+        /// <summary>
+        /// Anything that needs to be done before the plugin is unloaded should be done in this function
+        /// </summary>
+        public virtual void Unload()
+        {
+
+        }
     }
 }
