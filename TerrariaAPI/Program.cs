8f0de49d6b4109e2816096c894fbee324052a02b
high <pooop828@hotmail.com>
2011-05-24 22:53:09 -0400

    added solution

---
+++ TerrariaAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+
+namespace TerrariaAPI
+{
+    public static class Program
+    {
+
+        public static void Initialize(Main main)
+        {
+
+        }
+        public static void DeInitialize(Main main)
+        {
+        }
+    }
+}
