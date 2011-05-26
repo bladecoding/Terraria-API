2368b6d3e410ec25f5e5ce17b4bddfb0334653bc
high <pooop828@hotmail.com>
2011-05-26 00:36:41 -0400

    Added BeginDraw, EndDraw, LoadContent, PreInitialize, PostInitialize Removed Draw

--- TerrariaAPI/Hooks/GameHooks.cs
+++ TerrariaAPI/Hooks/GameHooks.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TerrariaAPI.Hooks
 {
@@ -14,11 +16,43 @@ namespace TerrariaAPI.Hooks
             if (OnUpdate != null)
                 OnUpdate(time);
         }
-        public static event Action<GameTime> OnDraw;
-        public static void Draw(GameTime time)
+        public static event Action<ContentManager> OnLoadContent;
+        public static void LoadContent(ContentManager manager)
         {
-            if (OnDraw != null)
-                OnDraw(time);
+            if (OnLoadContent != null)
+                OnLoadContent(manager);
+        }
+
+        /// <summary>
+        /// Called right after SpriteBatch.Begin
+        /// </summary>
+        public static event Action<GameTime, SpriteBatch> OnBeginDraw;
+        public static void BeginDraw(GameTime time, SpriteBatch batch)
+        {
+            if (OnBeginDraw != null)
+                OnBeginDraw(time, batch);
+        }
+        /// <summary>
+        /// Called right before SpriteBatch.End
+        /// </summary>
+        public static event Action<GameTime, SpriteBatch> OnEndDraw;
+        public static void EndDraw(GameTime time, SpriteBatch batch)
+        {
+            if (OnEndDraw != null)
+                OnEndDraw(time, batch);
+        }
+
+        public static event Action OnPreInitialize;
+        public static void PreInitialize()
+        {
+            if (OnPreInitialize != null)
+                OnPreInitialize();
+        }
+        public static event Action OnPostInitialize;
+        public static void PostInitialize()
+        {
+            if (OnPostInitialize != null)
+                OnPostInitialize();
         }
     }
 }
