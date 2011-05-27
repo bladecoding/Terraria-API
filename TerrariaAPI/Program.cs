3377b47b4f9df287b5006752a8b3361e0ddded2b
high <pooop828@hotmail.com>
2011-05-27 04:15:30 -0400

    Added basic cs loading

--- TerrariaAPI/Program.cs
+++ TerrariaAPI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.CSharp;
 using Terraria;
 
 namespace TerrariaAPI
@@ -17,10 +19,15 @@ namespace TerrariaAPI
         public static void Initialize(Main main)
         {
             if (!Directory.Exists(PluginsPath))
-                return;
+            {
+                Directory.CreateDirectory(PluginsPath);
+            }
+
+            //Not loaded for some reason :s
+            Assembly.Load("System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
 
             bool error = false;
-
             foreach (var f in new DirectoryInfo(PluginsPath).GetFiles("*.dll"))
             {
                 var asm = Assembly.LoadFile(f.FullName);
@@ -34,14 +41,63 @@ namespace TerrariaAPI
                         }
                         catch (Exception e)
                         {
-                            File.WriteAllText(f.Name + "_ErrorLog.txt", "Exception while trying to load: " + f.Name + Environment.NewLine + e.Message + Environment.NewLine + "Stack trace: " + Environment.NewLine + e.StackTrace);
+                            File.AppendAllText("ErrorLog.txt", "Exception while trying to load: " + f.Name + Environment.NewLine + e.Message + Environment.NewLine + "Stack trace: " + Environment.NewLine + e.StackTrace);
                             error = true;
                         }
                     }
                 }
             }
+            
+            
+            foreach (var f in new DirectoryInfo(PluginsPath).GetFiles("*.cs"))
+            {
+                var prov = new CSharpCodeProvider(new Dictionary<string, string>() { { "CompilerVersion", "v4.0" } });
+                var cp = new CompilerParameters();
+                cp.GenerateInMemory = true;
+                cp.GenerateExecutable = false;
+                cp.CompilerOptions = "/d:TERRARIA_API /unsafe";
 
-            if(error)
+                foreach (var a in assemblies)
+                {
+                    if (!cp.ReferencedAssemblies.Contains(a.Location))
+                        cp.ReferencedAssemblies.Add(a.Location);
+                }
+                var r = prov.CompileAssemblyFromSource(cp, File.ReadAllText(f.FullName));
+                if (r.Errors.Count > 0)
+                {
+                    for (int i = 0; i < r.Errors.Count; i++)
+                    {
+                        File.AppendAllText("ErrorLog.txt",
+                                          "Error compiling: " + f.Name + Environment.NewLine + "Line number " + r.Errors[i].Line +
+                                             ", Error Number: " + r.Errors[i].ErrorNumber +
+                                             ", '" + r.Errors[i].ErrorText + ";" +
+                                             Environment.NewLine + Environment.NewLine);
+                        error = true;
+                    }
+                }
+                else
+                {
+                    foreach (var t in r.CompiledAssembly.GetTypes())
+                    {
+                        if (t.BaseType == typeof(TerrariaPlugin))
+                        {
+                            try
+                            {
+                                Plugins.Add((TerrariaPlugin)Activator.CreateInstance(t, main));
+                            }
+                            catch (Exception e)
+                            {
+                                File.AppendAllText("ErrorLog.txt",
+                                                  "Exception while trying to load: " + f.Name + Environment.NewLine +
+                                                  e.Message + Environment.NewLine + "Stack trace: " +
+                                                  Environment.NewLine + e.StackTrace);
+                                error = true;
+                            }
+                        }
+                    }
+                }
+            }
+            if (error)
                 MessageBox.Show("There were errors while loading the mods, check the error logs for more details", "Terraria API", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
         public static void DeInitialize()
