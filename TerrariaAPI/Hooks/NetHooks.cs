56b0f17500bdda47a11af63c6b112df6cd0cefac
high <pooop828@hotmail.com>
2011-05-30 04:13:35 -0400

    added OnGreetPlayer

--- TerrariaAPI/Hooks/NetHooks.cs
+++ TerrariaAPI/Hooks/NetHooks.cs
@@ -6,57 +6,8 @@ using Terraria;
 
 namespace TerrariaAPI.Hooks
 {
-    public class AllowEventArgs : EventArgs
-    {
-        public bool Allow { get; set; }
-        public AllowEventArgs()
-        {
-            Allow = true;
-        }
-    }
-    public class HandledEventArgs : EventArgs
-    {
-        public bool Handled { get; set; }
-        public HandledEventArgs()
-        {
-            Handled = false;
-        }
-    }
-    public static class ServerHooks
-    {
-        /// <summary>
-        /// arg1 = WhoAmI
-        /// </summary>
-        public static event Action<int, AllowEventArgs> OnJoin;
-        public static bool Join(int whoami)
-        {
-            var args = new AllowEventArgs();
-            if (OnJoin != null)
-                OnJoin(whoami, args);
-            return args.Allow;
-        }
-        /// <summary>
-        /// arg1 = WhoAmI
-        /// </summary>
-        public static event Action<int> OnLeave;
-        public static void Leave(int whoami)
-        {
-            if (OnLeave != null)
-                OnLeave(whoami);
-        }
-
-        /// <summary>
-        /// arg1 = WhoAmI, arg2 = Message
-        /// </summary>
-        public static event Action<int, string, HandledEventArgs> OnChat;
-        public static bool Chat(int whoami, string msg)
-        {
-            var args = new HandledEventArgs();
-            if (OnChat != null)
-                OnChat(whoami, msg, args);
-            return args.Handled;
-        }
-    }
+    
+    
 
     public static class NetHooks
     {
@@ -88,5 +39,14 @@ namespace TerrariaAPI.Hooks
             return args.Handled;
         }
 
+        public delegate void GreetPlayerD(int who, HandledEventArgs e);
+        public static event GreetPlayerD OnGreetPlayer;
+        public static bool GreetPlayer(int who)
+        {
+            var args = new HandledEventArgs();
+            if (OnGreetPlayer != null)
+                OnGreetPlayer(who, args);
+            return args.Handled;
+        }
     }
 }
