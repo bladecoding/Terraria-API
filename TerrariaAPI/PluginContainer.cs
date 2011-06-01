6a97a6e90a1b1b315066876d5ef4700bf0f24784
high <pooop828@hotmail.com>
2011-05-31 21:47:07 -0400

    You think after missing a file twice that I would learn. NOPE

---
+++ TerrariaAPI/PluginContainer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TerrariaAPI
+{
+    public class PluginContainer : IDisposable
+    {
+        public TerrariaPlugin Plugin { get; protected set; }
+        public bool Initialized { get; protected set; }
+        public PluginContainer(TerrariaPlugin plugin)
+        {
+            Plugin = plugin;
+            Initialized = false;
+        }
+
+        public void Initialize()
+        {
+            Plugin.Initialize();
+            Initialized = true;
+        }
+        public void DeInitialize()
+        {
+            Plugin.DeInitialize();
+            Initialized = false;
+        }
+
+        public void Dispose()
+        {
+            Plugin.Dispose();
+        }
+    }
+}
