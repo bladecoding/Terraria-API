901b5a9341bbdecec7c5b31fadd183f25ff18bef
high <pooop828@hotmail.com>
2011-06-06 16:39:05 -0400

    Added PlayerHooks UpdatePhysics hook

---
+++ TerrariaAPI/Hooks/PlayerHooks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+
+namespace TerrariaAPI.Hooks
+{
+    public static class PlayerHooks
+    {
+        /// <summary>
+        /// Called right before controls are handled
+        /// </summary>
+        public static event Action<Player> UpdatePhysics;
+
+        public static void OnUpdatePhysics(Player player)
+        {
+            if (UpdatePhysics != null)
+                UpdatePhysics(player);
+        }
+    }
+}
