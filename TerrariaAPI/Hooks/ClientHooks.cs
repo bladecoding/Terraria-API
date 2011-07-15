a6707a653a2f6602aea01866e1f5e43354ac09e4
Jaex <flexy123@gmail.com>
2011-07-15 05:26:05 +0300

    Added ChatReceived event, Writing chat messages to console

--- TerrariaAPI/Hooks/ClientHooks.cs
+++ TerrariaAPI/Hooks/ClientHooks.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using Microsoft.Xna.Framework;
 using Terraria;
 
 namespace TerrariaAPI.Hooks
@@ -7,7 +10,37 @@ namespace TerrariaAPI.Hooks
     {
         static ClientHooks()
         {
+            NetHooks.GetData += NetHooks_GetData;
             NetHooks.SendData += NetHooks_SendData;
+        }
+
+        private static void NetHooks_GetData(GetDataEventArgs e)
+        {
+            if (Main.netMode != 2 && e.MsgID == PacketTypes.ChatText && e.Length > 3)
+            {
+                byte playerID = e.Msg.readBuffer[e.Index];
+                Color color = new Color(e.Msg.readBuffer[e.Index + 1] << 16, e.Msg.readBuffer[e.Index + 2] << 8, e.Msg.readBuffer[e.Index + 3]);
+                string message = Encoding.ASCII.GetString(e.Msg.readBuffer, e.Index + 4, e.Length - 5);
+                OnChatReceived(playerID, color, message);
+            }
+        }
+
+        public delegate void ChatReceivedEventHandler(byte playerID, Color color, string message);
+        public static event ChatReceivedEventHandler ChatReceived;
+
+        public static void OnChatReceived(byte playerID, Color color, string message)
+        {
+#if CLIENT
+            if (playerID < 255 && Main.player[playerID].active)
+            {
+                Console.WriteLine("{0}: {1}", Main.player[playerID].name, message);
+            }
+#endif
+
+            if (ChatReceived != null)
+            {
+                ChatReceived(playerID, color, message);
+            }
         }
 
         private static void NetHooks_SendData(SendDataEventArgs e)
@@ -25,11 +58,14 @@ namespace TerrariaAPI.Hooks
 
         public static bool OnChat(ref string msg)
         {
-            if (Chat == null)
-                return false;
-            var args = new HandledEventArgs();
-            Chat(ref msg, args);
-            return args.Handled;
+            if (Chat != null)
+            {
+                HandledEventArgs args = new HandledEventArgs();
+                Chat(ref msg, args);
+                return args.Handled;
+            }
+
+            return false;
         }
     }
 }
